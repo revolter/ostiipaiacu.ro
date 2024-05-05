2480e1f8cbe2e85f3a3651fd1dbafe4cc3934cc5
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2024-05-05 23:01:03 +0300

    Refactor: Enabled the possibility to create posts with non-exclamation answers

--- _plugins/jekyll-answerify.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-    module AnswerifyFilter
-        def answerify(answer)
-            return "#{answer.capitalize}!"
-        end
-    end
-end
-
-Liquid::Template.register_filter(Jekyll::AnswerifyFilter)
